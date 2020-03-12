751ba7598517db66362df52ee27328020f736bd6
anatoliy-savchak <anatoliy.savchak@gmail.com>
2020-03-12 17:22:42 +0200

    Shadow Warren update 2

---
+++ data/scr/py06120_cormyr_sw_daemon.py
@@ -0,0 +1,17 @@
+from toee import *
+from debugg import *
+from utils_obj import *
+from const_toee import *
+
+# DAEMON
+def cormyr_sw_san_new_map( attachee, triggerer ):
+	do_hook_doors()
+	return SKIP_DEFAULT
+
+def do_hook_doors():
+	#breakp("do_hook_doors")
+	for obj in game.obj_list_range(game.party[0].location, 200, OLC_PORTAL ):
+		assert isinstance(obj, PyObjHandle)
+		obj_scripts_clear(obj)
+		obj.scripts[sn_use] = 6112
+	return SKIP_DEFAULT
