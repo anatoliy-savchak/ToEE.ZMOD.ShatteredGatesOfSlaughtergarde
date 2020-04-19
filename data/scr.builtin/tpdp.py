b0a7d7b355db9c9e262badc7b0600c83685bbce3
anatoliy_savchak <anatoliy_savchak@epam.com>
2020-04-19 08:31:44 +0300

    napping impl 1

--- data/scr.builtin/tpdp.py
+++ data/scr.builtin/tpdp.py
@@ -5,7 +5,7 @@ class TurnBasedStatus:
 		self.hourglass_state = toee.D20ACT_Full_Round_Action
 		self.num_bonus_attacks = 0
 		self.surplus_move_dist = 0
-		self.flags = 0
+		self.flags = 0 # TurnBasedStatusFlags
 		self.attack_mode_code = 0
 		return
 
@@ -39,4 +39,76 @@ class D20Action:
 		return
 
 	def to_hit_processing(self, projHndl, thrownItem):
+		return
+
+class BonusList:
+	def __init__(self):
+		# init 0; 0x1 - overallCapHigh set; 0x2 - overallCapLow set; 0x4 - force cap override (otherwise it can only impose restrictions i.e. it will only change the cap if it's lower than the current one)
+		self.flags = 0
+		return
+
+	def add(self, value, bonType, mesline):
+		""" bonus_list.add(int: value, int: bonType, int: mesline) -> None """
+		return
+	
+	def add(self, value, bonType, text):
+		""" bonus_list.add(int: value, int: bonType, str: text) -> None """
+		return
+
+	def add_from_feat(self, value, bonType, mesline, feat):
+		""" bonus_list.add_from_feat(int: value, int: bonType, int: mesline, int: feat) -> int """
+		return 0
+
+	def set_overall_cap(self, bonflags, newCap, newCapType, bonusMesline):
+		""" bonus_list.set_overall_cap(int: bonflags, int: newCap, int: newCapType, int: bonusMesline) -> None """
+		return
+
+	def modify(self, value, bonType, meslineIdentifier):
+		""" bonus_list.modify(int: value, int: bonType, int: meslineIdentifier) -> int """
+		return 0
+
+	def modify(self, value, bonType, meslineIdentifier):
+		""" bonus_list.modify(int: value, int: bonType, int: meslineIdentifier) -> int """
+		return 0
+
+	def get_sum(self, mesline):
+		""" bonus_list.get_sum() -> int """
+		return 0
+
+	def get_total(self, mesline):
+		""" bonus_list.get_sum() -> int """
+		return 0
+
+	def add_cap(self, bonType, value, mesline):
+		""" Adds cap for a particular bonus type
+		bonus_list.add_cap(int: bonType, int: value, int: mesline) -> None """
+		return
+
+	def add_cap(self, bonType, value, mesline, text):
+		""" Adds cap for a particular bonus type
+		bonus_list.add_cap(int: bonType, int: value, int: mesline, int: text) -> None """
+		return
+
+class AttackPacket:
+	def __init__(self):
+		self.attacker = toee.PyObjHandle()
+		self.target = toee.PyObjHandle()
+		self.action_type = toee.D20A_UNSPECIFIED_MOVE
+		self.event_key = 0 # dispKey
+		return
+
+	def get_weapon_used(self):
+		""" attack_packet.get_weapon_used() -> PyObjHandle """
+		return toee.PyObjHandle()
+
+	def is_offhand_attack(self):
+		""" attack_packet.is_offhand_attack() -> int """
+		return 0
+
+	def get_flags(self):
+		""" attack_packet.get_flags() -> int """
+		return toee.D20CAF_HIT
+
+	def set_flags(self, flagsNew):
+		""" attack_packet.set_flags(int: flagsNew) -> None """
 		return
