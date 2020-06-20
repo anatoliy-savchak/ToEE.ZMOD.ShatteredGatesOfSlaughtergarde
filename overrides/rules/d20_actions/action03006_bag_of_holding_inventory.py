9e9797005234d9099b57c89e69dcb9cb390cfe7b
anatoliy_savchak <anatoliy_savchak@epam.com>
2020-06-21 00:31:50 +0300

    bh update 1

---
+++ overrides/rules/d20_actions/action03006_bag_of_holding_inventory.py
@@ -0,0 +1,17 @@
+﻿import toee, tpactions, debug
+
+def GetActionName():
+	return "Inspect"
+
+def GetActionDefinitionFlags():
+	return toee.D20ADF_Python
+	
+def GetTargetingClassification():
+	return toee.D20TC_Target0
+
+def GetActionCostType():
+	return toee.D20ACT_Move_Action
+
+def AddToSequence(d20action, action_seq, tb_status):
+	action_seq.add_action(d20action)
+	return toee.AEC_OK
