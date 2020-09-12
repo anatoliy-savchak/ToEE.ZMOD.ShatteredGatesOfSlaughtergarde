9b3816d85c1de51e6f15236160acb8f99b55b787
anatoliy_savchak <anatoliy_savchak@epam.com>
2020-09-12 12:04:03 +0300

    Shiningted update 6
    Wands

---
+++ overrides/scr/py00680Cleric.py
@@ -0,0 +1,22 @@
+from toee import *
+from utilities import *
+from scripts import *
+
+def san_dialog( attachee, triggerer ):
+	attachee.spells_pending_to_memorized()
+	attachee.turn_towards(triggerer)
+	if not attachee.has_met( triggerer ):
+		triggerer.begin_dialog( attachee, 1 )
+		return SKIP_DEFAULT
+	else:
+		triggerer.begin_dialog( attachee, 100 )
+	return SKIP_DEFAULT
+
+def san_first_heartbeat( attachee, triggerer ):
+	return RUN_DEFAULT
+
+def san_dying( attachee, triggerer ):
+	return RUN_DEFAULT
+
+def san_resurrect( attachee, triggerer ):
+	return RUN_DEFAULT
